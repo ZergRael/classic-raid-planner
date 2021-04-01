f09b0914159d014cbb81fee4cd86bbedbfbe4cb3
zergrael <zergrael@gmail.com>
2021-04-01 18:12:01 +0200

    Added export to clipboard button

--- src/components/Exporter.vue
+++ src/components/Exporter.vue
@@ -2,6 +2,12 @@
   <div>
     <h2 class="text-xl">Export</h2>
     <textarea class="w-1/2 border" readonly v-model="output"></textarea>
+    <button
+      class="bg-green-100 rounded p-1 m-1 align-middle"
+      @click="exportToClipboard"
+    >
+      Copy
+    </button>
   </div>
 </template>
 
@@ -13,6 +19,11 @@ export default {
       get() {
         return this.$store.getters.rawPlayers.join("\n");
       },
+    },
+  },
+  methods: {
+    exportToClipboard() {
+      navigator.clipboard.writeText(this.output);
     },
   },
 };
